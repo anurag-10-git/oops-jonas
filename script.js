4227d92e70098d759ffc459ad293f42a3c6994a3
anurag-10-git <anuragmehra2011@gmail.com>
2022-11-12 18:02:46 +0530

    constructors

--- script.js
+++ script.js
@@ -29,16 +29,61 @@ Person.prototype.calcAge =  function() {
 }
 
 console.log(Person.prototype);
-console.log(Object.getPrototypeOf(Person));
 console.log(Object.getPrototypeOf(matilda));
 
 jonas.calcAge();
 console.log(jonas.__proto__);
-console.log(Person.prototype);
-console.log(jonas.prototype);
+console.dir(jonas.prototype);
 
 console.log(Person.prototype === jonas.__proto__);
 
 Person.prototype.species = 'Homo sapiens';
 
-console.log(jonas.species);
+console.log(jonas.__proto__.__proto__);
+console.dir(Person.prototype);
+console.dir(Object.getPrototypeOf(Person));
+
+Array.prototype.unique = function() {
+        return [...new Set(this)];
+}
+
+
+// console.log([1,1,2,2,2,2,3,3,3].unique());
+
+// function onlyUnique(value, index, self) {
+//         console.log(self.indexOf(value),index);
+//         return self.indexOf(value) === index;
+//       }
+      
+//       // usage example:
+//       var a = [1,1,2,2,2,2,3,3,3];
+//       var unique = a.filter(onlyUnique);
+      
+//       console.log(unique); 
+
+const Car = function(make, speed) {
+        this.make = make;
+        this.speed = speed;
+}
+
+Car.prototype.accelerate = function() {
+        this.speed+=10;
+        console.log(`the speed of ${this.make} after acceleration ${this.speed} kmph`);
+}
+
+Car.prototype.brake = function() {
+        this.speed-=5;
+        console.log(`the speed of ${this.make} after brake ${this.speed} kmph`);
+}
+
+let car1 = new Car('BMW', 120);
+let car2 = new Car('Mercedes', 95);
+
+car1.accelerate();
+car1.accelerate();
+car1.accelerate();
+car1.accelerate();
+car1.accelerate();
+car2.accelerate();
+car1.brake();
+car2.brake();
